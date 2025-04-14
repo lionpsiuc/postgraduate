a9f7dec7dfa263308851f32ba34da3583dba0c80
Ion Lipsiuc <ionlipsiuc@gmail.com>
2025-04-14 17:05:14 +0100

    A file containing all MPI functions covered in High-Performance Computing Software II

---
+++ hilary-term/software/functions.c
@@ -0,0 +1,158 @@
+int MPI_Allgather(void* sendbuf, int sendcount, MPI_Datatype sendtype,
+                  void* recvbuf, int recvcount, MPI_Datatype recvtype,
+                  MPI_Comm comm);
+
+int MPI_Allreduce(void* send_buffer, void* recv_buffer, int count,
+                  MPI_Datatype datatype, MPI_Op op, MPI_Comm comm);
+
+int MPI_Alltoall(void* sendbuf, int sendcount, MPI_Datatype sendtype,
+                 void* recvbuf, int recvcnt, MPI_Datatype recvtype,
+                 MPI_Comm comm);
+
+int MPI_Bcast(void* buffer, int count, MPI_Datatype datatype, int root,
+              MPI_Comm comm);
+
+int MPI_Cart_shift(MPI_Comm comm, int direction, int displ, int* src,
+                   int* dest);
+
+int MPI_Comm_rank(MPI_Comm comm, int* rank);
+
+int MPI_Comm_size(MPI_Comm comm, int* size);
+
+int MPI_Comm_spawn(const char* command, char* argv[], int maxprocs,
+                   MPI_Info info, int root, MPI_Comm comm, MPI_Comm* intercomm,
+                   int array_of_errcodes[]);
+
+int MPI_Comm_split(MPI_Comm comm, int colour, int key, MPI_Comm* newcomm);
+
+int MPI_Finalize();
+
+int MPI_Gather(void* send_buffer, int send_count, MPI_Datatype sendtype,
+               void* recv_buffer, int recv_count, MPI_Datatype recvtype,
+               int root, MPI_Comm comm);
+
+int MPI_Init(int* argc, char*** argv);
+
+int MPI_Init_thread(int* argc, char*** argv, int required, int* provided);
+
+int MPI_Irecv(void* buf, int count, MPI_Datatype datatype, int source, int tag,
+              MPI_Comm comm, MPI_Request* request);
+
+int MPI_Isend(void* buf, int count, MPI_Datatype datatype, int dest, int tag,
+              MPI_Comm comm, MPI_Request* request);
+
+int MPI_Recv(void* buf, int count, MPI_Datatype datatype, int source, int tag,
+             MPI_Comm comm, MPI_Status* status);
+
+int MPI_Reduce(void* sendbuf, void* recvbuf, int count, MPI_Datatype datatype,
+               MPI_Op op, int root, MPI_Comm comm);
+
+int MPI_Scatter(void* sendbuf, int sendcnt, MPI_Datatype sendtype,
+                void* recvbuf, int recvcnt, MPI_Datatype recvtype, int root,
+                MPI_Comm comm);
+
+int MPI_Send(void* buf, int count, MPI_Datatype datatype, int dest, int tag,
+             MPI_Comm comm);
+
+int MPI_Sendrecv(void* sendbuf, int sendcount, MPI_Datatype sendtype, int dest,
+                 int sendtag, void* recvbuf, int recvcount,
+                 MPI_Datatype recvtype, int source, int recvtag, MPI_Comm comm,
+                 MPI_Status* status);
+
+int MPI_Test(MPI_Request* request, int* flag, MPI_Status* status);
+
+int MPI_Type_commit(MPI_Datatype* datatype);
+
+int MPI_Type_contiguous(int count, MPI_Datatype oldtype, MPI_Datatype* newtype);
+
+int MPI_Type_free(MPI_Datatype* datatype);
+
+int MPI_Type_struct(int count, int* array_of_blocklengths,
+                    MPI_Aint*     array_of_displacements,
+                    MPI_Datatype* array_of_types, MPI_Datatype* newtype);
+
+int MPI_Type_vector(int count, int blocklength, int stride,
+                    MPI_Datatype oldtype, MPI_Datatype* newtype);
+
+int MPI_Type_size(MPI_Datatype datatype, int* size);
+
+int MPI_Type_get_extent(MPI_Datatype datatype, MPI_Aint* lb, MPI_Aint* extent);
+
+int MPI_Wait(MPI_Request* request, MPI_Status* status);
+
+int MPI_Win_create(void* base, MPI_Aint size, int disp_unit, MPI_Info info,
+                   MPI_Comm comm, MPI_Win* win);
+
+int MPI_Put(const void* origin_addr, int origin_count,
+            MPI_Datatype origin_datatype, int target_rank, MPI_Aint target_disp,
+            int target_count, MPI_Datatype target_datatype, MPI_Win win);
+
+int MPI_Get(void* origin_addr, int origin_count, MPI_Datatype origin_datatype,
+            int target_rank, MPI_Aint target_disp, int target_count,
+            MPI_Datatype target_datatype, MPI_Win win);
+
+int MPI_Accumulate(const void* origin_addr, int origin_count,
+                   MPI_Datatype origin_datatype, int target_rank,
+                   MPI_Aint target_disp, int target_count,
+                   MPI_Datatype target_datatype, MPI_Op op, MPI_Win win);
+
+int MPI_Fetch_and_op(const void* origin_addr, void* result_addr,
+                     MPI_Datatype datatype, int target_rank,
+                     MPI_Aint target_disp, MPI_Op op, MPI_Win win);
+
+int MPI_Win_fence(int assert, MPI_Win win);
+
+int MPI_Win_lock(int lock_type, int rank, int assert, MPI_Win win);
+
+int MPI_Win_unlock(int rank, MPI_Win win);
+
+int MPI_Win_start(MPI_Group group, int assert, MPI_Win win);
+
+int MPI_Win_complete(MPI_Win win);
+
+int MPI_Win_post(MPI_Group group, int assert, MPI_Win win);
+
+int MPI_Win_wait(MPI_Win win);
+
+int MPI_File_open(MPI_Comm comm, const char* filename, int amode, MPI_Info info,
+                  MPI_File* fh);
+
+int MPI_File_read(MPI_File fh, void* buf, int count, MPI_Datatype datatype,
+                  MPI_Status* status);
+
+int MPI_File_write(MPI_File fh, const void* buf, int count,
+                   MPI_Datatype datatype, MPI_Status* status);
+
+int MPI_File_read_at(MPI_File fh, MPI_Offset offset, void* buf, int count,
+                     MPI_Datatype datatype, MPI_Status* status);
+
+int MPI_File_write_at(MPI_File fh, MPI_Offset offset, const void* buf,
+                      int count, MPI_Datatype datatype, MPI_Status* status);
+
+int MPI_File_read_all(MPI_File fh, void* buf, int count, MPI_Datatype datatype,
+                      MPI_Status* status);
+
+int MPI_File_write_all(MPI_File fh, const void* buf, int count,
+                       MPI_Datatype datatype, MPI_Status* status);
+
+int MPI_File_seek(MPI_File fh, MPI_Offset offset, int whence);
+
+int MPI_File_set_view(MPI_File fh, MPI_Offset disp, MPI_Datatype etype,
+                      MPI_Datatype filetype, const char* datarep,
+                      MPI_Info info);
+
+int MPI_Type_create_resized(MPI_Datatype oldtype, MPI_Aint lb, MPI_Aint extent,
+                            MPI_Datatype* newtype);
+
+int MPI_Type_create_struct(int count, int array_of_blocklengths[],
+                           const MPI_Aint     array_of_displacements[],
+                           const MPI_Datatype array_of_types[],
+                           MPI_Datatype*      newtype);
+
+int MPI_File_close(MPI_File* fh);
+
+int MPI_Info_create(MPI_Info* info);
+
+int MPI_Info_free(MPI_Info* info);
+
+int MPI_Info_set(MPI_Info info, char* key, char* value);
